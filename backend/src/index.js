80be0aa067df3e473f441e810b57e7a70ca0fce6
Gustavo Mafra <lgmafra@gmail.com>
2020-03-26 01:37:22 -0300

    created ong controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  return res.json({ hello: 'world' });
-});
+app.use(routes);
 
 app.listen(3333);
